196763a7ae233960f8939a4fee0fb0a267bfb6bf
Shivkalp <2022bit027@sggs.ac.in>
2024-04-22 09:20:38 +0530

    Initialization Block - init block

---
+++ InitializationBlock.java
@@ -0,0 +1,24 @@
+class Hello{
+   static
+    {
+    System.out.println("in static block of hello");
+    }
+    {
+    System.out.println("in init block of hello");
+    }
+}
+
+
+ public class InitializationBlock{
+   {
+    System.out.println("in init block");
+    }
+    static
+    {
+    System.out.println("in static block");
+    }
+    public static void main(String ...args){
+     InitializationBlock i = new InitializationBlock();
+     Hello obj = new Hello();
+     }
+     }
