7e0b17ef366448f9910ed379c531cf69e8038f0a
Shivkalp <2022bit027@sggs.ac.in>
2024-04-25 22:03:38 +0530

    Exception handling using try and catch block

---
+++ Practice1.java
@@ -0,0 +1,23 @@
+class Practice1{
+   public static void main(String [] args){
+      try{
+         Thread.sleep(1000);
+	 System.out.println("inside try");
+	 Thread.sleep(1000);
+	 System.out.println(args[0]);
+	 Thread.sleep(1000);
+	 System.out.println("Inside try2");
+	 }
+	 
+    
+       catch(ArrayIndexOutOfBoundsException e){ 
+          System.out.println("inside catch2");
+       }
+       catch(InterruptedException e){  //this is a compile time exception
+	System.out.println("InterruptedException found");
+	}
+        catch(Exception e){
+          System.out.println("inside catch");
+       }
+       }
+       }
