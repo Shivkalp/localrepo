f4568981002053873ee296ba0a985da0945d6b76
Shivkalp <2022bit027@sggs.ac.in>
2024-04-30 19:50:46 +0530

    Packages...

---
+++ PackageExample.java
@@ -0,0 +1,21 @@
+// Everything regarding packages.
+
+import java.lang.*;
+//import java.util.Scanner;
+
+class PackageExample{
+
+   public static void main(String ...args){
+      System.out.println("Hi, how are you ?");
+      //Scanner sc = new Scanner(System.in);
+        //sc.nextInt();
+      java.util.Scanner sc = new java.util.Scanner(System.in);
+      sc.nextInt();
+     
+      
+      // the program will be terminated after the execution of the statement on 11th line.
+      // if we comment out the Scanner class, which has been imported, then, there will be an error.
+      
+      
+      }
+      }
