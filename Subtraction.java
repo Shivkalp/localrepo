79ad2f247aeac0a18577a2d509ed3e3063262f63
Shivkalp <2022bit027@sggs.ac.in>
2024-05-01 18:54:10 +0530

    How to create user-defined packages

---
+++ Subtraction.java
@@ -0,0 +1,15 @@
+package pack;
+
+public class Subtraction{
+    
+        private double d1, d2;
+       public Subtraction(double a, double b){
+            d1 = a;
+            d2 = b;
+        }
+
+      public void sub(){
+            System.out.println("The sub is "+(d1-d2));
+            
+        }
+}
