8d28b78f02fa6998fe27648f52feab50776288f7
Shivkalp <2022bit027@sggs.ac.in>
2024-05-06 22:22:49 +0530

    concat method

---
+++ Concat.java
@@ -0,0 +1,14 @@
+class Concat{
+   public static void main(String args[]){
+      String exam = "sggs";
+      String exam1 = exam.concat("");
+      if(exam == exam1){
+      System.out.println("both are same");
+      }
+      else{
+      System.out.println("both are not same");
+      }
+      }
+      }
+
+      // concat method.
