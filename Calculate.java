9895d134aadbf085aa5126d5c534bab24a67fff3
Shivkalp <2022bit027@sggs.ac.in>
2024-05-10 20:37:33 +0530

    Calculating electricity bills - Abstract Classes

---
+++ Calculate.java
@@ -0,0 +1,39 @@
+// Abstract classes in java.
+
+abstract class Plan{
+   double rate;
+   public void calculateBill(int units){
+      System.out.println("The total bill is :"+rate*units);
+      }
+   
+   public abstract void getRate();
+   
+   }
+   
+class CommercialPlan extends Plan{
+   public void getRate(){
+      rate = 5.00;
+      }
+      }
+      
+class DomesticPlan extends Plan{
+    public void getRate(){
+      rate = 2.00;
+      }
+      }
+      
+class Calculate{;
+   public static void main(String ...args){
+   Plan ref;
+   ref = new CommercialPlan();
+   ref.getRate();
+   ref.calculateBill(20);
+   
+   ref = new DomesticPlan();
+   ref.getRate();
+   ref.calculateBill(30);
+   
+   System.out.println("This is all about abstract classes");
+   
+   }
+   }
